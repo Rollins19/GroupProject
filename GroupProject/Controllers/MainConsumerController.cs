8ac1026814093e761d46b5b82f5ed3514721a455
John Rollins <jar5@EMAIL.USCUPSTATE.EDU>
2018-11-08 13:17:38 -0500

    Updated Connection string and changed main consumer view

---
+++ GroupProject/Controllers/MainConsumerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Data;
+using System.Net;
+using GroupProject.Models;
+
+namespace GroupProject.Controllers
+{
+    public class MainConsumerController : Controller
+    {
+          private Group2DBEntities db = new Group2DBEntities();
+        // GET: MainConsumer
+        public ActionResult MainConsumer()
+        {
+               ViewBag.DepartureDate = new SelectList(db.FlightInfoes, "FlightNum", "DepartureDate.date");
+               ViewBag.ArrivalAirport = new SelectList(db.FlightInfoes, "FlightNum", "ArrivalAirport");
+               ViewBag.DepartureTime = new SelectList(db.FlightInfoes, "FlightNum", "DepartureTime"); //Need to modify flight capacity to available seat capacity
+               return View();
+        }
+    }
+}
