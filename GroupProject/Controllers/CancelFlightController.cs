af074f1e49fde3d3dc15667aa88c33b58e19c4be
mhaake5 <43859544+mhaake5@users.noreply.github.com>
2018-11-14 23:18:36 -0500

    MyTickets updated with the flight information for now.

---
+++ GroupProject/Controllers/CancelFlightController.cs
@@ -0,0 +1,19 @@
+﻿using GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupProject.Controllers
+{
+    public class CancelFlightController : Controller
+    {
+        GroupProject20181102032945_dbEntities db = new GroupProject20181102032945_dbEntities();
+        // GET: CancelFlight
+        public ActionResult CancelFlight()
+        {
+            return View();
+        }
+    }
+}
