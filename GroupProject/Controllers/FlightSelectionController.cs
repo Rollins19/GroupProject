226f5531c320fb650f805d545011e213af33e2a9
John Rollins <jar5@EMAIL.USCUPSTATE.EDU>
2018-11-15 11:14:00 -0500

    Added "Next" buttom to flightselection

---
+++ GroupProject/Controllers/FlightSelectionController.cs
@@ -0,0 +1,22 @@
+﻿using GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupProject.Controllers
+{
+    public class FlightSelectionController : Controller
+    {     
+
+          private GroupProject20181102032945_dbEntities db = new GroupProject20181102032945_dbEntities();
+
+          // GET: FlightSelection
+          public ActionResult FlightSelection()
+          {
+               var flightInfo = db.FlightInfoes.ToList();
+               return View("FlightSelection", flightInfo);
+          }
+    }
+}
