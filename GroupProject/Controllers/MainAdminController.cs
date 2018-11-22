d9fd05a106e265e260258da3e187128f7fe0ede2
John Rollins <jar5@EMAIL.USCUPSTATE.EDU>
2018-11-21 23:22:19 -0500

    Created the main admin controller and view. Tweaked look of a few pages like print tickets and check flight status.

---
+++ GroupProject/Controllers/MainAdminController.cs
@@ -0,0 +1,41 @@
+﻿using GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupProject.Controllers
+{
+    public class MainAdminController : Controller
+    {
+        // GET: MainAdmin
+      
+        private GroupProject20181102032945_dbEntities1 db = new GroupProject20181102032945_dbEntities1();
+
+        // public string ArrivalAirport { get; private set; }
+
+        // GET: MainConsumer
+        [Authorize]
+        public ActionResult MainAdmin()
+        {
+             ViewBag.DepartureDate = new SelectList(db.FlightInfoes, "FlightNum", "DepartureDate.date");
+             ViewBag.ArrivalAirport = new SelectList(db.FlightInfoes, "FlightNum", "ArrivalAirport");
+             ViewBag.FlightCapacity = new SelectList(db.FlightInfoes, "FlightNum", "FlightCapacity");
+             ViewBag.DepartureDateList = new SelectList( GetFlightInfoList(), "FlightNum", "DepartureDate");
+             ViewBag.ArrivalAirportList = new SelectList(GetFlightInfoList(), "FlightNum", "ArrivalAirport");
+             ViewBag.FlightCapacityList = new SelectList(GetFlightInfoList(), "FlightNum", "FlightCapacity");
+             ViewBag.FlightCapacityList = new SelectList(GetFlightInfoList(), "FlightNum", "FlightCapacity");
+       
+             return View();
+        }
+
+        [Authorize]
+        public List<FlightInfo> GetFlightInfoList()
+        {
+             GroupProject20181102032945_dbEntities1 db = new GroupProject20181102032945_dbEntities1();
+             List<FlightInfo> flightInfoes = db.FlightInfoes.ToList();
+             return flightInfoes;
+        }
+     }
+}
