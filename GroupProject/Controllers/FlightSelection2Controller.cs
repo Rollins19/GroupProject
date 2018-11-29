3b12936916311583253046eaf632313676616319
Vishalkumar Patel <vrpatel@EMAIL.USCUPSTATE.EDU>
2018-11-29 17:52:49 -0500

    Created FlightSelection

---
+++ GroupProject/Controllers/FlightSelection2Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GroupProject.Models;
+
+namespace GroupProject.Controllers
+{
+    public class FlightSelection2Controller : Controller
+    {
+        private GroupProject20181102032945_dbEntities1 db = new GroupProject20181102032945_dbEntities1();
+
+        // GET: FlightSelection2
+        [Authorize]
+        public ActionResult FlightSelection2()
+        {
+            return View(db.FlightInfoes.ToList());
+        }
+    }
+}
